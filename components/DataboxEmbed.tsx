256f2c15ca2942cfbbf59305b0499fd05c6eef07
Vector Marketing <vmcdigital@gmail.com>
2026-08-21 12:16:05 -0700

    Create DataboxEmbed.tsx

---
+++ components/DataboxEmbed.tsx
@@ -0,0 +1,15 @@
+export default function DataboxEmbed() {
+  return (
+    <div className="databox-embed-wrapper">
+      <iframe
+        title="Vector META Ads Performance Dashboard"
+        src="https://app.databox.com/datawall/f690169e32a1b9aaf8dd120e3a95a95a4980acc69e90bb5"
+        frameBorder="0"
+        width="100%"
+        height="1200"
+        allowFullScreen
+        className="databox-iframe"
+      />
+    </div>
+  );
+}
