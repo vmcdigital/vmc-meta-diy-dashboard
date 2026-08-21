172599ad04b771d0010b24998523ed94567df289
Vector Marketing <vmcdigital@gmail.com>
2026-08-21 12:17:36 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import GettingStarted from '@/components/GettingStarted';
+
+export default function Home() {
+  return (
+    <section className="page-section">
+      <h2 className="page-title">1. Getting Started</h2>
+      <GettingStarted />
+    </section>
+  );
+}
